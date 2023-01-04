bdd1686b8815d53fafcf26eff0f4ebcf7f8d7de2
Harshal141 <harshalmukundapatil@gmail.com>
2023-01-04 22:09:55 +0530

    bohot sara code

--- js/app.js
+++ js/app.js
@@ -14,52 +14,7 @@ link.forEach(function (link) {
         })
 })
 
-(function timer() {
-  const second = 1000,
-    minute = second * 60,
-    hour = minute * 60,
-    day = hour * 24;
 
-
-  let today = new Date(),
-    dd = String(today.getDate()).padStart(2, "0"),
-    mm = String(today.getMonth() + 1).padStart(2, "0"),
-    yyyy = today.getFullYear(),
-    nextYear = yyyy,
-    dayMonth = "3/11/",
-    birthday = dayMonth + yyyy;
-
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
-
-  const countDown = new Date(birthday).getTime(),
-    x = setInterval(function () {
-
-      const now = new Date().getTime(),
-        distance = countDown - now;
-          // console.log(distance/(day));
-      document.getElementById("days").innerText = pad(Math.floor(distance / (day)));
-      document.getElementById("hours").innerText = pad(Math.floor((distance % (day)) / (hour)));
-      document.getElementById("minutes").innerText = pad(Math.floor((distance % (hour)) / (minute)));
-      document.getElementById("seconds").innerText = pad(Math.floor((distance % (minute)) / second));
-
-      //do something later when date is reached
-      if (distance < 0) {
-        document.getElementById("headline").innerText = "Event is over now!";
-        document.getElementById("countdown").style.display = "none";
-        document.getElementById("content").style.display = "block";
-        clearInterval(x);
-      }
-      //seconds
-    }, 0)
-}());
-
-function pad(number) {
-  return ("0" + number).slice(-2);
-}
 // images carousel
 const track = document.getElementById("image-track");
 track.addEventListener("mouseover",(e)=>{
